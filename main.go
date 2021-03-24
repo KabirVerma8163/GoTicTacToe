6d564ad5303edf4f3b6eb93684c447e8c7f62600
KabirVerma8163 <kabir.verma.8163@gmail.com>
2021-03-23 21:58:50 -0400

    PvP working, actually

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import(
 	"os/exec"
 	// "strings"
 	// "strconv"
+	// "time"
 )
 
-func drawBoard (pieces [9]string){
+func drawBoard (pieces [9]string, player int){
+	fmt.Println("\nThis is how we do the Tic and the Tac with the Toe")
+	fmt.Printf("It is player %d's turn \n", player)
+	fmt.Println()
 	for i, s := range pieces{
 		i += 1
 		if s == ""{
@@ -26,38 +30,41 @@ func drawBoard (pieces [9]string){
 	}
 }
 
-func getBox(pieces [9]string)int{
-	correctInput := true
+func getBox(pieces [9]string, player int)int{
+	boxInLimit := false
 	var index int
-	drawBoard(pieces)
 
-	for correctInput {
+	for !boxInLimit {
+		drawBoard(pieces, player)
 		fmt.Println("Which number would you like to change?")
-		fmt.Print("Give your number here: ")
-		fmt.Scanln("%d", &index)
-		fmt.Println(index)
+		fmt.Printf("Give your number here: ")
+
+		fmt.Scanln(&index)
+		c := exec.Command("clear")
+    c.Stdout = os.Stdout
+    c.Run()
 		if index != 0 && index >= 1 && index <= 9{
-			break
+			boxInLimit = true
 		}
 	}
 	return index
 }
 
 func main (){
+	fmt.Println("\nWelcome To Tic Tac Toe")
 	var pieces [9]string
 	turn := 0
+	player := 1
 
 	for true {
-		fmt.Println("\nWelcome To Tic Tac Toe")
-		index := getBox(pieces)
+		index := getBox(pieces, player)
 		if turn%2 == 0{
 			pieces[index - 1] = "X"
+			player = 2
 		} else {
 			pieces[index - 1] = "O"
+			player = 1
 		}
 		turn ++
-		c := exec.Command("clear")
-    c.Stdout = os.Stdout
-    c.Run()
 	}
 }
