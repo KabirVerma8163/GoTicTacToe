001b41007e214898cd72a4e390439be1cbb69afc
KabirVerma8163 <kabir.verma.8163@gmail.com>
2021-03-24 00:05:18 -0400

    Error Catchin

--- main.go
+++ main.go
@@ -30,27 +30,58 @@ func drawBoard (pieces [9]string, player int){
 	}
 }
 
-func getBox(pieces [9]string, player int)int{
-	boxInLimit := false
-	var index int
+func checkWinner(pieces [9]string)bool{
+	for i := 0; i <= 2; i+=1{
+		if pieces[i] == pieces[i+3] && pieces[i] == pieces[i+6]{
+			return true
+		}
+	}
+	for i := 0; i <= 2; i+=3{
+		if pieces[i] == pieces[i+1] && pieces[i] == pieces[i+2]{
+			return true
+		}
+	}
+	return false
+}
 
-	for !boxInLimit {
+func getBox(pieces [9]string, player int)int{
+	turn, boxInLimit, boxNotFilled := 0, false, false
+	var index int
+	boxInLimitErr := "The value given for the box is out of bounds.\n"
+	boxNotFilledErr := "The box you provided was already filled in a previous move.\n"
+	var errString string
+
+
+	for !(boxInLimit && boxNotFilled) {
 		drawBoard(pieces, player)
+		if turn > 0{
+			errString = ""
+			if !boxInLimit {errString += boxInLimitErr} else if !boxNotFilled {errString += boxNotFilledErr}
+		}
+
 		fmt.Println("Which number would you like to change?")
+		if errString != "" {fmt.Print(errString)}
 		fmt.Printf("Give your number here: ")
 
 		fmt.Scanln(&index)
 		c := exec.Command("clear")
     c.Stdout = os.Stdout
     c.Run()
+		turn++
 		if index != 0 && index >= 1 && index <= 9{
 			boxInLimit = true
-		}
+			if pieces[index - 1] == ""{
+				boxNotFilled = true
+			}
+		}	else{	boxInLimit = false}
 	}
 	return index
 }
 
 func main (){
+	c := exec.Command("clear")
+	c.Stdout = os.Stdout
+	c.Run()
 	fmt.Println("\nWelcome To Tic Tac Toe")
 	var pieces [9]string
 	turn := 0
